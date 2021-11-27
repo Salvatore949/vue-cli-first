f80c04fa39e111d593bc6c4a27a3fb74816de015
Salvatore949 <salvo.mess49@gmail.com>
2021-11-27 17:09:46 +0100

    esercizio completo

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App!!!"/>
-  </div>
+  <Title />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Title from './components/Title.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Title,
   }
 }
 </script>
 
-<style lang="scss">
+<style lang="css">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
